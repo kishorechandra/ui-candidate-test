dfbe5d763d9ca48c19d3beefb1a7b0c7e2a13262
kishorechandra <yuvakishore.chandra@gmail.com>
2015-07-23 01:57:24 -0600

    Update jsanddom-test.js
    ATTENTION <KISHORE CHANDRA>

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,14 +1,69 @@
 /********************************
 Unit Test Example
 ********************************/
-module( "Example Unit Test" );
+
 test( "Example Test", 2, function() {
     // Verify the method exists
     equal( typeof divide, 'function', 'Must contain a divide function' ); 
 
     // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+    equal( divide( 4, 2 ), 2);
 });
 /********************************
 Please create your tests below...
 ********************************/
+
+test( "reverse String", 2, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' ); 
+
+    // Make sure the result from the divide function is valid
+    equal( reverseString( "john" ), "nhoj");
+});
+
+module( "minimum value Unit Test" );
+test( "minimum value", 2, function(assert) {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' ); 
+	var data = [1, 2, 3, 4, 5];
+    // Make sure the result from the divide function is valid
+     equal(findMinValue(data),1);
+});
+
+
+module( "splitListStrUsingComma Unit Test" );
+test( "minimum value", 2, function(assert) {
+    // Verify the method exists
+    equal( typeof splitListStrUsingComma, 'function', 'Must contain a findMinValue function' ); 
+	var data = "";
+    // Make sure the result from the divide function is valid
+     equal(splitListStrUsingComma(data),'[]');
+	 
+});
+
+module( "isOnlyWhitespace Unit Test" );
+test( "isOnlyWhitespace", 2, function(assert) {
+    // Verify the method exists
+    equal( typeof isOnlyWhitespace, 'function', 'Must contain a isOnlyWhitespace function' ); 
+	var data = " ";
+    // Make sure the result from the divide function is valid
+     equal(isOnlyWhitespace(data),true);
+	 
+});
+
+module( "sum Unit Test" );
+test( "sum", 2, function(assert) {
+    // Verify the method exists
+    equal( typeof sum, 'function', 'Must contain a sum function' ); 
+	var data = " ";
+    // Make sure the result from the divide function is valid
+     equal(sum(3,6),9);
+	 
+});
+
+
+
+
+
+
+
